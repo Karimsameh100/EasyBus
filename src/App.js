94a2cd7b9cb6dad240de13fdd3dd331fff0e9098
“3EnasMelad” <https://github.com/3EnasMelad>
2024-08-22 01:27:05 +0300

    About page for logedin user

--- src/App.js
+++ src/App.js
@@ -16,11 +16,10 @@ import CompleteComReg from "./Pages/CompleteComReg";
 import Login from "./Pages/Login";
 import Footer from "./Componants/Footer";
 import CompanyManagement from "./Pages/CompanyManagment";
-<<<<<<< HEAD
+
 import ReviewForm from "./Pages/CreateReview";
-=======
 import About from "./Pages/About";
->>>>>>> aboutTRIPTRACK
+
 // import SignUp from "./Pages/SignUp";
 
 function App() {
