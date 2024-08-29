b4ff0ccd1fb3e57b767cdebe70bc8df75cd0dbf4
“3EnasMelad” <https://github.com/3EnasMelad>
2024-08-29 12:51:59 +0300

    to update information about user profile and save changes

--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,22 +1,21 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Link, useNavigate } from "react-router-dom";
 
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link, useNavigate } from 'react-router-dom';
-
-function  TripTrackSignup() {
+function TripTrackSignup() {
   const [selectedRole, setSelectedRole] = useState();
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
 
   const handleRoleChange = (event) => {
     setSelectedRole(event.target.value);
   };
 
   const handleJoinClick = () => {
-    if (selectedRole === 'client') {
-      navigate('/client');  
-    } else if (selectedRole === 'company') {
-      navigate('/CompanySignup');  
+    if (selectedRole === "client") {
+      navigate("/client");
+    } else if (selectedRole === "company") {
+      navigate("/CompanySignup");
     }
   };
 
@@ -27,17 +26,20 @@ function  TripTrackSignup() {
       <Form>
         <div className="row">
           <div className="col-md-6 text-center mb-4">
-            <div className="p-4 border border-dark rounded" style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+            <div
+              className="p-4 border border-dark rounded"
+              style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+            >
               <Form.Group controlId="clientRadio">
                 <Form.Label>
                   <Form.Check
                     name="role"
                     type="radio"
                     value="client"
-                    checked={selectedRole === 'client'}
+                    checked={selectedRole === "client"}
                     onChange={handleRoleChange}
                   />
-                  <br/>
+                  <br />
                   <span>I'm a client, appling for job</span>
                 </Form.Label>
               </Form.Group>
@@ -45,17 +47,20 @@ function  TripTrackSignup() {
           </div>
 
           <div className="col-md-6 text-center">
-            <div className="p-4 border border-dark rounded" style={{ fontSize: '1.5rem', fontWeight: 'bold' }}>
+            <div
+              className="p-4 border border-dark rounded"
+              style={{ fontSize: "1.5rem", fontWeight: "bold" }}
+            >
               <Form.Group controlId="companyRadio">
                 <Form.Label>
                   <Form.Check
                     name="role"
                     type="radio"
                     value="company"
-                    checked={selectedRole === 'company'}
+                    checked={selectedRole === "company"}
                     onChange={handleRoleChange}
                   />
-                  <br/>
+                  <br />
                   <span>I'm a company, looking for work</span>
                 </Form.Label>
               </Form.Group>
@@ -64,14 +69,19 @@ function  TripTrackSignup() {
         </div>
 
         <div className="text-center mt-3">
-          <Button variant="primary" onClick={handleJoinClick} disabled={!selectedRole}>
-            {selectedRole === 'client' ? 'Join as a Client' : 'Join as a Company'}
+          <Button
+            variant="primary"
+            onClick={handleJoinClick}
+            disabled={!selectedRole}
+          >
+            {selectedRole === "client"
+              ? "Join as a Client"
+              : "Join as a Company"}
           </Button>
         </div>
       </Form>
 
       <p className="text-center mt-3">
-
         Already have an account? <Link to={"/login"}>Log In</Link>
       </p>
     </div>
@@ -79,7 +89,3 @@ function  TripTrackSignup() {
 }
 
 export default TripTrackSignup;
-
-
-
-
--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -1,7 +1,141 @@
+// import React, { useState, useEffect } from "react";
+// import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+// import { Link, useNavigate } from "react-router-dom";
+// import { FaSignOutAlt, FaEdit } from "react-icons/fa"; // Importing icons
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// const UserProfile = () => {
+//   const [profilePic, setProfilePic] = useState(null);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     // Load user data from localStorage
+//     const storedName = localStorage.getItem("userName");
+//     const storedEmail = localStorage.getItem("userEmail");
+//     const storedProfilePic = localStorage.getItem("userProfilePic");
+
+//     if (storedName) setName(storedName);
+//     if (storedEmail) setEmail(storedEmail);
+//     if (storedProfilePic) setProfilePic(storedProfilePic);
+//   }, []);
+
+//   const handleImageUpload = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onloadend = () => {
+//         const imageUrl = reader.result;
+//         setProfilePic(imageUrl);
+//         localStorage.setItem("userProfilePic", imageUrl); // Save image URL to localStorage
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   //   function handleLogout() {
+//   //     localStorage.removeItem("userInputs");
+//   //     navigate("/");
+//   //   }
+//   const handleLogout = () => {
+//     // Clear user data from localStorage
+//     localStorage.removeItem("isLoggedIn");
+//     localStorage.removeItem("userName");
+//     localStorage.removeItem("userEmail");
+//     localStorage.removeItem("userProfilePic");
+
+//     // Redirect to home page
+//     navigate("/");
+//   };
+
+//   //   const handleEditAccount = () => {
+//   //     // Redirect to registration form
+//   //     navigate("/client");
+//   //   };
+
+//   const handleEditAccount = () => {
+//     // Redirect to registration form with user data
+//     navigate("/client", { state: { name, email, profilePic } });
+//   };
+
+//   return (
+//     <Container
+//       className="my-5"
+//       style={{ backgroundColor: "#f8f9fa", minHeight: "150vh" }}
+//     >
+//       <Row className="justify-content-center">
+//         <Col md={12}>
+//           <Card>
+//             <Row noGutters>
+//               <Col
+//                 md={4}
+//                 className="d-flex justify-content-center align-items-center"
+//               >
+//                 <Card.Img
+//                   variant="top"
+//                   src={profilePic || "https://via.placeholder.com/150"}
+//                   style={{
+//                     width: "150px",
+//                     height: "150px",
+//                     borderRadius: "50%",
+//                   }}
+//                 />
+//               </Col>
+//               <Col md={8}>
+//                 <Card.Body>
+//                   <Card.Title>Welcome :{name}</Card.Title>
+//                   <Card.Subtitle className="mb-2 text-muted">
+//                     Your Mail :{email}
+//                   </Card.Subtitle>
+//                   <Form>
+//                     <Form.Group controlId="formFile">
+//                       <Form.Control
+//                         type="file"
+//                         style={{ marginTop: "5vh" }}
+//                         onChange={handleImageUpload}
+//                       />
+//                     </Form.Group>
+//                     <Button type="submit" style={{ marginTop: "5vh" }}>
+//                       Save Changes
+//                     </Button>
+//                   </Form>
+//                   <div
+//                     className="d-flex justify-content-end"
+//                     style={{ marginTop: "2vh" }}
+//                   >
+//                     <Button
+//                       variant="outline-danger"
+//                       onClick={handleLogout}
+//                       style={{ marginRight: "1vh" }}
+//                     >
+//                       <FaSignOutAlt style={{ marginRight: "5px" }} />
+//                       Logout
+//                     </Button>
+//                     <Button
+//                       variant="outline-success"
+//                       onClick={handleEditAccount}
+//                     >
+//                       <FaEdit style={{ marginRight: "5px" }} />
+//                       Edit Account
+//                     </Button>
+//                   </div>
+//                 </Card.Body>
+//               </Col>
+//             </Row>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default UserProfile;
+// ---------------------------------------------kkkkkkkkkkkkkkkkkkkkk
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
-import { FaSignOutAlt, FaEdit } from "react-icons/fa"; // Importing icons
+import { useNavigate } from "react-router-dom";
+import { FaSignOutAlt, FaEdit } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const UserProfile = () => {
@@ -11,7 +145,6 @@ const UserProfile = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Load user data from localStorage
     const storedName = localStorage.getItem("userName");
     const storedEmail = localStorage.getItem("userEmail");
     const storedProfilePic = localStorage.getItem("userProfilePic");
@@ -28,35 +161,25 @@ const UserProfile = () => {
       reader.onloadend = () => {
         const imageUrl = reader.result;
         setProfilePic(imageUrl);
-        localStorage.setItem("userProfilePic", imageUrl); // Save image URL to localStorage
+        localStorage.setItem("userProfilePic", imageUrl);
       };
       reader.readAsDataURL(file);
     }
   };
 
-  //   function handleLogout() {
-  //     localStorage.removeItem("userInputs");
-  //     navigate("/");
-  //   }
   const handleLogout = () => {
-    // Clear user data from localStorage
     localStorage.removeItem("isLoggedIn");
     localStorage.removeItem("userName");
     localStorage.removeItem("userEmail");
     localStorage.removeItem("userProfilePic");
 
-    // Redirect to home page
     navigate("/");
   };
 
-  //   const handleEditAccount = () => {
-  //     // Redirect to registration form
-  //     navigate("/client");
-  //   };
-
   const handleEditAccount = () => {
-    // Redirect to registration form with user data
-    navigate("/client", { state: { name, email, profilePic } });
+    navigate("/client", {
+      state: { name, email, profilePic, isEditMode: true },
+    });
   };
 
   return (
@@ -84,9 +207,9 @@ const UserProfile = () => {
               </Col>
               <Col md={8}>
                 <Card.Body>
-                  <Card.Title>Welcome :{name}</Card.Title>
+                  <Card.Title>Welcome : {name}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">
-                    Your Mail :{email}
+                    Your Mail : {email}
                   </Card.Subtitle>
                   <Form>
                     <Form.Group controlId="formFile">
