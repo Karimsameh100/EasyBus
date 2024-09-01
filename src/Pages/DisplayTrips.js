b781da8c7b8baf37b9b232166ed2206c11113ed8
mmetyas <magedmetyas1@gmail.com>
2024-09-02 01:52:55 +0300

    return

--- src/Pages/DisplayTrips.js
+++ src/Pages/DisplayTrips.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { EditTripModal, DeleteTripModal } from '../Componants/EditDeleteModel.js';
 import { Modal, ModalTitle, ModalHeader, ModalBody, ModalFooter,Button } from 'react-bootstrap';
