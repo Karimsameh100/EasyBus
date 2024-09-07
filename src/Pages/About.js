4729fe2c67e4dcda76175c01861ad0f7fe0a1b77
“3EnasMelad” <https://github.com/3EnasMelad>
2024-09-07 15:09:22 +0300

    list booking trips

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,10 +1,12 @@
+// -------------------------------------------------------------
+// تعديلات بسيطه bootstrap
 // import React from "react";
 // import { Link } from "react-router-dom";
 // import aboutImg from "../logo/about.jpg";
 // import img3 from "../logo/DALL·E 2024-08-22 00.11.23 - A modern and well-maintained public transportation service, featuring a fleet of buses traveling across various Egyptian governorates. The image shoul.webp";
 // import img1 from "../logo/DALL·E 2024-08-22 00.10.15 - A modern, comfortable bus interior with plush, blue leather seats and large windows, showing a clean and organized setting. The image should convey a .webp";
+// import img2 from "../logo/third.webp";
 
-// import img2 from "../logo/about1.jfif";
 // const About = () => {
 //   // const handleClick = () => {
 //   //   window.location.hash = "#Home";
@@ -12,46 +14,35 @@
 //   // };
 //   return (
 //     <>
-//       <div style={{ textAlign: "center" }} className="container">
-//         <h1
-//           id="about"
-//           className="text-center "
-//           style={{ color: "#003366", marginTop: "10vh" }}
-//         >
+//       <div className="container text-center my-5">
+//         <h1 id="about" className="text-primary my-5">
 //           About TRIP TRACK
 //         </h1>
 //         <img
 //           src={aboutImg}
-//           style={{ width: "130vh", height: "70vh", marginTop: "5vh" }}
-//           all
+//           className="img-fluid my-5"
+//           style={{ maxWidth: "100%", height: "auto" }}
+//           alt="About TRIP TRACK"
 //         />
-//         <p
-//           style={{
-//             marginRight: "10vh",
-//             marginLeft: "10vh",
-//             marginTop: "5vh",
-//             marginBottom: "10vh",
-//           }}
-//         >
-//           <h5>
-//             <b>TRIP TRACK</b> is a premier public transport company, providing
-//             luxurious yet affordable long distance travel in Egypt connecting
-//             Greater Cairo with the most famous destinations along The Nile River
-//             and both The Mediterranean & Red sea. As the Middle Eastern most
-//             promising transportation company, we believe in making a difference
-//             through our values. We aspire to make the traveling experience with
-//             us a memorable and joyful one. <b>TRIP TRACK</b> adopts technology
-//             that enriches our customer experience, from booking till reaching
-//             destinations. Our highly trained staff deliver a luxurious service.
-//             As well as, our professional drivers ensure safe and punctual trips,
-//             with the quality our passengers deserve.
-//           </h5>
+//         <p className="mx-auto my-5" style={{ maxWidth: "800px" }}>
+//           <strong>TRIP TRACK</strong> is a premier public transport company,
+//           providing luxurious yet affordable long distance travel in Egypt
+//           connecting Greater Cairo with the most famous destinations along The
+//           Nile River and both The Mediterranean & Red sea. As the Middle Eastern
+//           most promising transportation company, we believe in making a
+//           difference through our values. We aspire to make the traveling
+//           experience with us a memorable and joyful one.{" "}
+//           <strong>TRIP TRACK</strong> adopts technology that enriches our
+//           customer experience, from booking till reaching destinations. Our
+//           highly trained staff deliver a luxurious service. As well as, our
+//           professional drivers ensure safe and punctual trips, with the quality
+//           our passengers deserve.
 //         </p>
 //       </div>
 //       <hr />
-//       <div className="container my-5" style={{ marginTop: "12vh" }}>
+//       <div className="container my-5">
 //         <div className="text-center mb-4">
-//           <h2 style={{ marginBottom: "8vh" }}>Our Mission</h2>
+//           <h2 className="mb-4">Our Mission</h2>
 //         </div>
 //         <div className="row text-center">
 //           <div className="col-lg-4 col-md-6 mb-4">
@@ -84,18 +75,18 @@
 //           </div>
 //         </div>
 //         <div className="text-center mt-4">
-//           <Link to={"/ "}>
+//           <Link to={"/"}>
 //             <button
 //               // onClick={handleClick}
 //               id="infoBTN"
-//               className="btn  btn-outline-dark me-3"
+//               className="btn btn-outline-dark me-3"
 //             >
 //               Book Now
 //             </button>
 //           </Link>
 //           {/* <a
 //             href="#"
-//             className="btn btn-primary   btn-lg px-5"
+//             className="btn btn-primary btn-lg px-5"
 //             style={{ marginBottom: "10vh" }}
 //           >
 //             Book Now
@@ -105,21 +96,37 @@
 //     </>
 //   );
 // };
-// export default About;
 
-// تعديلات بسيطه bootstrap
-import React from "react";
+// export default About;
+// --------------------------------------------
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import aboutImg from "../logo/about.jpg";
 import img3 from "../logo/DALL·E 2024-08-22 00.11.23 - A modern and well-maintained public transportation service, featuring a fleet of buses traveling across various Egyptian governorates. The image shoul.webp";
 import img1 from "../logo/DALL·E 2024-08-22 00.10.15 - A modern, comfortable bus interior with plush, blue leather seats and large windows, showing a clean and organized setting. The image should convey a .webp";
-import img2 from "../logo/about1.jfif";
+import img2 from "../logo/third.webp";
+import { Button, Form } from "react-bootstrap";
 
 const About = () => {
-  // const handleClick = () => {
-  //   window.location.hash = "#Home";
-  //   window.scrollTo({ top: 0, behavior: "smooth" });
-  // };
+  const [validated, setValidated] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    } else {
+      setShowMessage(true);
+    }
+    setValidated(true);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <>
       <div className="container text-center my-5">
@@ -182,23 +189,74 @@ const About = () => {
             </p>
           </div>
         </div>
+
         <div className="text-center mt-4">
           <Link to={"/"}>
-            <button
-              // onClick={handleClick}
-              id="infoBTN"
-              className="btn btn-outline-dark me-3"
-            >
+            <button id="infoBTN" className="btn btn-outline-dark me-3">
               Book Now
             </button>
           </Link>
-          {/* <a
-            href="#"
-            className="btn btn-primary btn-lg px-5"
-            style={{ marginBottom: "10vh" }}
-          >
-            Book Now
-          </a> */}
+        </div>
+      </div>
+
+      {/* Recommendation Form */}
+      <div className="container my-5">
+        <h3 className="text-center mb-4">Recommendation Form</h3>
+        <Form
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+          className="recommendation-form mx-auto"
+          style={{ maxWidth: "500px" }}
+        >
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter your username"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid username.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="email" className="mt-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              required
+              type="email"
+              placeholder="Enter your email"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group controlId="message" className="mt-3">
+            <Form.Label>Message</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={3}
+              required
+              placeholder="Enter your message"
+            />
+            <Form.Control.Feedback type="invalid">
+              Please provide a message.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button type="submit" className="mt-3">
+            Submit
+          </Button>
+        </Form>
+        {showMessage && (
+          <div className="mt-3 alert alert-success text-center">
+            Thank you for your recommendation!
+          </div>
+        )}
+        {/* Scroll to Top Button */}
+        <div className="text-center my-4">
+          <Button variant="secondary" onClick={scrollToTop}>
+            Scroll to Top
+          </Button>
         </div>
       </div>
     </>
--- src/Pages/UserProfile.js
+++ src/Pages/UserProfile.js
@@ -1,25 +1,175 @@
+// import React, { useState, useEffect } from "react";
+// import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+// import { useNavigate } from "react-router-dom";
+// import { FaSignOutAlt, FaEdit } from "react-icons/fa";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { confirmAlert } from "react-confirm-alert";
+// import "react-confirm-alert/src/react-confirm-alert.css"; // Import the CSS for the confirm alert
+
+// const UserProfile = () => {
+//   const [profilePic, setProfilePic] = useState(null);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const storedName = localStorage.getItem("userName");
+//     const storedEmail = localStorage.getItem("userEmail");
+//     const storedProfilePic = localStorage.getItem("userProfilePic");
+
+//     if (storedName) setName(storedName);
+//     if (storedEmail) setEmail(storedEmail);
+//     if (storedProfilePic) setProfilePic(storedProfilePic);
+//   }, []);
+
+//   const handleImageUpload = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onloadend = () => {
+//         const imageUrl = reader.result;
+//         setProfilePic(imageUrl);
+//         localStorage.setItem("userProfilePic", imageUrl);
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   const handleLogout = () => {
+//     confirmAlert({
+//       title: "Confirm Logout",
+//       message: "Are you sure you want to logout?",
+//       buttons: [
+//         {
+//           label: "Yes",
+//           onClick: () => {
+//             localStorage.removeItem("isLoggedIn");
+//             localStorage.removeItem("userName");
+//             localStorage.removeItem("userEmail");
+//             localStorage.removeItem("userProfilePic");
+//             navigate("/");
+//           },
+//         },
+//         {
+//           label: "No",
+//         },
+//       ],
+//       closeOnClickOutside: true,
+//     });
+//   };
+
+//   const handleEditAccount = () => {
+//     navigate("/client", {
+//       state: { name, email, profilePic, isEditMode: true },
+//     });
+//   };
+
+//   return (
+//     <Container
+//       className="my-5"
+//       style={{ backgroundColor: "#f8f9fa", minHeight: "150vh" }}
+//     >
+//       <Row className="justify-content-center">
+//         <Col md={12}>
+//           <Card>
+//             <Row noGutters>
+//               <Col
+//                 md={4}
+//                 className="d-flex justify-content-center align-items-center"
+//               >
+//                 <Card.Img
+//                   variant="top"
+//                   src={profilePic || "https://via.placeholder.com/150"}
+//                   style={{
+//                     width: "150px",
+//                     height: "150px",
+//                     borderRadius: "50%",
+//                   }}
+//                 />
+//               </Col>
+//               <Col md={8}>
+//                 <Card.Body>
+//                   <Card.Title>Welcome : {name}</Card.Title>
+//                   <Card.Subtitle className="mb-2 text-muted">
+//                     Your Mail : {email}
+//                   </Card.Subtitle>
+//                   <Form>
+//                     <Form.Group controlId="formFile">
+//                       <Form.Control
+//                         type="file"
+//                         style={{ marginTop: "5vh" }}
+//                         onChange={handleImageUpload}
+//                       />
+//                     </Form.Group>
+//                     <Button type="submit" style={{ marginTop: "5vh" }}>
+//                       Save Changes
+//                     </Button>
+//                   </Form>
+//                   <div
+//                     className="d-flex justify-content-end"
+//                     style={{ marginTop: "2vh" }}
+//                   >
+//                     <Button
+//                       variant="outline-danger"
+//                       onClick={handleLogout}
+//                       style={{ marginRight: "1vh" }}
+//                     >
+//                       <FaSignOutAlt style={{ marginRight: "5px" }} />
+//                       Logout
+//                     </Button>
+//                     <Button
+//                       variant="outline-success"
+//                       onClick={handleEditAccount}
+//                     >
+//                       <FaEdit style={{ marginRight: "5px" }} />
+//                       Edit Account
+//                     </Button>
+//                   </div>
+//                 </Card.Body>
+//               </Col>
+//             </Row>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default UserProfile;
+// ========================================================================111
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Card, Button, Form } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+  Alert,
+} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { FaSignOutAlt, FaEdit } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { confirmAlert } from "react-confirm-alert";
-import "react-confirm-alert/src/react-confirm-alert.css"; // Import the CSS for the confirm alert
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 const UserProfile = () => {
   const [profilePic, setProfilePic] = useState(null);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [bookedTrips, setBookedTrips] = useState([]); // new state for trips
   const navigate = useNavigate();
 
   useEffect(() => {
     const storedName = localStorage.getItem("userName");
     const storedEmail = localStorage.getItem("userEmail");
     const storedProfilePic = localStorage.getItem("userProfilePic");
+    const storedTrips = JSON.parse(localStorage.getItem("bookedTrips")) || []; //ّ-----------------
 
     if (storedName) setName(storedName);
     if (storedEmail) setEmail(storedEmail);
     if (storedProfilePic) setProfilePic(storedProfilePic);
+    setBookedTrips(storedTrips); // set trips
   }, []);
 
   const handleImageUpload = (e) => {
@@ -47,6 +197,7 @@ const UserProfile = () => {
             localStorage.removeItem("userName");
             localStorage.removeItem("userEmail");
             localStorage.removeItem("userProfilePic");
+            localStorage.removeItem("bookedTrips");
             navigate("/");
           },
         },
@@ -105,6 +256,23 @@ const UserProfile = () => {
                       Save Changes
                     </Button>
                   </Form>
+
+                  {/* Display booked trips or a message if none */}
+                  <div style={{ marginTop: "2vh" }}>
+                    <h5>Your Booked Trips:</h5>
+                    {bookedTrips.length > 0 ? (
+                      <ul>
+                        {bookedTrips.map((trip, index) => (
+                          <li key={index}>{trip}</li>
+                        ))}
+                      </ul>
+                    ) : (
+                      <Alert variant="info">
+                        You have not booked any trips yet.
+                      </Alert>
+                    )}
+                  </div>
+
                   <div
                     className="d-flex justify-content-end"
                     style={{ marginTop: "2vh" }}
@@ -136,3 +304,199 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+// ================================================================pagination
+// import React, { useState, useEffect } from "react";
+// import {
+//   Container,
+//   Row,
+//   Col,
+//   Card,
+//   Button,
+//   Form,
+//   Alert,
+// } from "react-bootstrap";
+// import { useNavigate } from "react-router-dom";
+// import { FaSignOutAlt, FaEdit } from "react-icons/fa";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { confirmAlert } from "react-confirm-alert";
+// import "react-confirm-alert/src/react-confirm-alert.css";
+// import ReactPaginate from "react-paginate"; // استيراد مكتبة الباجنيشن
+
+// const UserProfile = () => {
+//   const [profilePic, setProfilePic] = useState(null);
+//   const [name, setName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [bookedTrips, setBookedTrips] = useState([]); // رحلات المحجوزة
+//   const [currentPage, setCurrentPage] = useState(0); // الحالة لحفظ الصفحة الحالية
+//   const tripsPerPage = 5; // عدد الرحلات في كل صفحة
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const storedName = localStorage.getItem("userName");
+//     const storedEmail = localStorage.getItem("userEmail");
+//     const storedProfilePic = localStorage.getItem("userProfilePic");
+//     const storedTrips = JSON.parse(localStorage.getItem("bookedTrips")) || [];
+
+//     if (storedName) setName(storedName);
+//     if (storedEmail) setEmail(storedEmail);
+//     if (storedProfilePic) setProfilePic(storedProfilePic);
+//     setBookedTrips(storedTrips); // تعيين الرحلات
+//   }, []);
+
+//   const handleImageUpload = (e) => {
+//     const file = e.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onloadend = () => {
+//         const imageUrl = reader.result;
+//         setProfilePic(imageUrl);
+//         localStorage.setItem("userProfilePic", imageUrl);
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   const handleLogout = () => {
+//     confirmAlert({
+//       title: "Confirm Logout",
+//       message: "Are you sure you want to logout?",
+//       buttons: [
+//         {
+//           label: "Yes",
+//           onClick: () => {
+//             localStorage.removeItem("isLoggedIn");
+//             localStorage.removeItem("userName");
+//             localStorage.removeItem("userEmail");
+//             localStorage.removeItem("userProfilePic");
+//             localStorage.removeItem("bookedTrips");
+//             navigate("/");
+//           },
+//         },
+//         {
+//           label: "No",
+//         },
+//       ],
+//       closeOnClickOutside: true,
+//     });
+//   };
+
+//   const handleEditAccount = () => {
+//     navigate("/client", {
+//       state: { name, email, profilePic, isEditMode: true },
+//     });
+//   };
+
+//   // حساب عدد الصفحات المطلوبة
+//   const pageCount = Math.ceil(bookedTrips.length / tripsPerPage);
+
+//   // تحديد الرحلات التي سيتم عرضها في الصفحة الحالية
+//   const displayedTrips = bookedTrips.slice(
+//     currentPage * tripsPerPage,
+//     (currentPage + 1) * tripsPerPage
+//   );
+
+//   // التعامل مع تغيير الصفحة
+//   const handlePageClick = (data) => {
+//     setCurrentPage(data.selected);
+//   };
+
+//   return (
+//     <Container
+//       className="my-5"
+//       style={{ backgroundColor: "#f8f9fa", minHeight: "150vh" }}
+//     >
+//       <Row className="justify-content-center">
+//         <Col md={12}>
+//           <Card>
+//             <Row noGutters>
+//               <Col
+//                 md={4}
+//                 className="d-flex justify-content-center align-items-center"
+//               >
+//                 <Card.Img
+//                   variant="top"
+//                   src={profilePic || "https://via.placeholder.com/150"}
+//                   style={{
+//                     width: "150px",
+//                     height: "150px",
+//                     borderRadius: "50%",
+//                   }}
+//                 />
+//               </Col>
+//               <Col md={8}>
+//                 <Card.Body>
+//                   <Card.Title>Welcome : {name}</Card.Title>
+//                   <Card.Subtitle className="mb-2 text-muted">
+//                     Your Mail : {email}
+//                   </Card.Subtitle>
+//                   <Form>
+//                     <Form.Group controlId="formFile">
+//                       <Form.Control
+//                         type="file"
+//                         style={{ marginTop: "5vh" }}
+//                         onChange={handleImageUpload}
+//                       />
+//                     </Form.Group>
+//                     <Button type="submit" style={{ marginTop: "5vh" }}>
+//                       Save Changes
+//                     </Button>
+//                   </Form>
+
+//                   {/* عرض الرحلات المحجوزة أو رسالة إذا لم يكن هناك أي رحلة */}
+//                   <div style={{ marginTop: "2vh" }}>
+//                     <h5>Your Booked Trips:</h5>
+//                     {bookedTrips.length > 0 ? (
+//                       <>
+//                         <ul>
+//                           {displayedTrips.map((trip, index) => (
+//                             <li key={index}>{trip}</li>
+//                           ))}
+//                         </ul>
+//                         {/* باجنيشن */}
+//                         <ReactPaginate
+//                           previousLabel={"Previous"}
+//                           nextLabel={"Next"}
+//                           pageCount={pageCount}
+//                           onPageChange={handlePageClick}
+//                           containerClassName={"pagination"}
+//                           activeClassName={"active"}
+//                         />
+//                       </>
+//                     ) : (
+//                       <Alert variant="info">
+//                         You have not booked any trips yet.
+//                       </Alert>
+//                     )}
+//                   </div>
+
+//                   <div
+//                     className="d-flex justify-content-end"
+//                     style={{ marginTop: "2vh" }}
+//                   >
+//                     <Button
+//                       variant="outline-danger"
+//                       onClick={handleLogout}
+//                       style={{ marginRight: "1vh" }}
+//                     >
+//                       <FaSignOutAlt style={{ marginRight: "5px" }} />
+//                       Logout
+//                     </Button>
+//                     <Button
+//                       variant="outline-success"
+//                       onClick={handleEditAccount}
+//                     >
+//                       <FaEdit style={{ marginRight: "5px" }} />
+//                       Edit Account
+//                     </Button>
+//                   </div>
+//                 </Card.Body>
+//               </Col>
+//             </Row>
+//           </Card>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
+
+// export default UserProfile;
