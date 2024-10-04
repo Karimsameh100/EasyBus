ed3e5f0dbaeab03f2df78a1a2136dc0a2667403d
“minanaser1” <menanaser8888@gmail.com>
2024-10-04 19:25:14 +0300

    handle error form

--- src/Pages/CompanySignup.js
+++ src/Pages/CompanySignup.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';  // Import Axios
-import logo from "../logo/bus.png";
+import logo from "../logo/trip.jpeg";
 
 function CompanySignup() {
     const [userInputs, setUserInputs] = useState({
